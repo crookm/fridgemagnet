486c346b2c916112d113e0ecc826600815b9462d
Matt Crook <mattlcrook@icloud.com>
2023-12-24 11:23:37 +1300

    build(deps): update to net8 (#13)

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 
-./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet workload install wasm-tools
 
